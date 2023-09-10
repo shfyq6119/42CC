21f3e68ea29607674a0eee71a24ea2efa8cd9476
M Syafik M Isa <mm-isa@c1r1s3.42singapore.sg>
2023-09-10 18:00:09 +0800

    update

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,33 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 13:23:15 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/09/09 13:31:54 by mm-isa           ###   ########.fr       */
+/*   Created: 2023/09/10 16:29:09 by mm-isa            #+#    #+#             */
+/*   Updated: 2023/09/10 16:47:35 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+int	ft_atoi(char *n)
 {
-	while (*str)
+	long	pol;
+	long	res_long;
+
+	pol = 1;
+	res_long = 0;
+	while (*n == 32)
+		n++;
+	if (*n == 45 || *n == 43)
 	{
-		if (*str == c)
-			return ((char *)str);
-		str++;
+		if (n* == 45)
+			pol *= -1;
+		n++;
 	}
-	if (c == '\0')
-		return ((char *)str);
-	return (0);
+	if (!(*n > 47 && *n < 58))
+		return (0);
+	while (*n > 47 & *n < 58)
+	{
+		res_long = res_long * 10 + (*n - 48);
+		n++;
+	}
+	return (res_long * pol);
 }
-/*
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-        if (argc != 1)
-                printf("%s\n", (ft_strchr(argv[1], 99)));
-        return (0);
-}*/
--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/09 13:23:15 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/09/09 13:31:54 by mm-isa           ###   ########.fr       */
+/*   Created: 2023/09/09 13:28:08 by mm-isa            #+#    #+#             */
+/*   Updated: 2023/09/09 13:33:13 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+int	ft_isascii(int c)
 {
-	while (*str)
-	{
-		if (*str == c)
-			return ((char *)str);
-		str++;
-	}
-	if (c == '\0')
-		return ((char *)str);
+	if (c >= 0 && c <= 127)
+		return (1);
 	return (0);
 }
-/*
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-        if (argc != 1)
-                printf("%s\n", (ft_strchr(argv[1], 99)));
-        return (0);
-}*/
