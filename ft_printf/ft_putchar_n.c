69b831947915f9ca7c1a38a6bf2c4a31338980fb
M Syafik M Isa <mm-isa@c2r4s15.42singapore.sg>
2023-10-06 15:41:36 +0800

    printf testing

--- ft_printf/ft_putchar_n.c
+++ ft_printf/ft_putchar_n.c
@@ -1,26 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libprintf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar_n.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/27 02:42:14 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/10/06 15:32:44 by mm-isa           ###   ########.fr       */
+/*   Created: 2023/10/06 12:24:35 by mm-isa            #+#    #+#             */
+/*   Updated: 2023/10/06 15:33:50 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBPRINTF_H
-# define LIBPRINTF_H
-# define HEXUP "0123456789ABCDEF"
-# define HEXLOW "0123456789abcdef"
-# include <stdarg.h>
-# include <unistd.h>
+#include "libprintf.h"
 
-int	ft_printf(const char *format, ...);
-int	ft_putchar_n(char c);
-int	ft_putstr_n(char *s);
-int	ft_putnbr_n(long i);
-int	ft_putaddr_n(unsigned long p);
-int	ft_puthex_n(unsigned long h, char *hexcase);
-
-#endif
+int	ft_putchar_n(char c)
+{
+	return (write(1, &c, 1));
+}
--- ft_printf/ft_putstr_n.c
+++ ft_printf/ft_putstr_n.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libprintf.h                                        :+:      :+:    :+:   */
+/*   ft_putstr_n.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/27 02:42:14 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/10/06 15:32:44 by mm-isa           ###   ########.fr       */
+/*   Created: 2023/10/06 12:30:29 by mm-isa            #+#    #+#             */
+/*   Updated: 2023/10/06 15:35:07 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBPRINTF_H
-# define LIBPRINTF_H
-# define HEXUP "0123456789ABCDEF"
-# define HEXLOW "0123456789abcdef"
-# include <stdarg.h>
-# include <unistd.h>
+#include "libprintf.h"
 
-int	ft_printf(const char *format, ...);
-int	ft_putchar_n(char c);
-int	ft_putstr_n(char *s);
-int	ft_putnbr_n(long i);
-int	ft_putaddr_n(unsigned long p);
-int	ft_puthex_n(unsigned long h, char *hexcase);
+int	ft_putstr_n(char *s)
+{
+	int	i;
 
-#endif
+	i = 0;
+	if (!s)
+		return (ft_putstr_n("(null)"));
+	while (s[i])
+		i += ft_putchar_n(s[i]);
+	return (i);
+}
