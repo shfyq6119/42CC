04dc08ed9abc8da25e95552297a2d52f78495df8
M Syafik M Isa <mm-isa@c2r4s15.42singapore.sg>
2023-11-05 21:33:40 +0800

    mac backup

--- ft_printf/ft_puthex_n.c
+++ ft_printf/ft_puthex_n.c
@@ -6,14 +6,19 @@
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:54:22 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/10/06 15:58:56 by mm-isa           ###   ########.fr       */
+/*   Updated: 2023/10/07 12:08:59 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_puthex_n(unsigned long h, char *hexcase)
 {
+	int	tally;
+
+	tally = 0;
 	if (h < 16)
 		return (ft_putchar_n(hexcase[h]));
-	return (ft_puthex_n(h / 16, hexcase) + ft_putchar_n(hexcase[h % 16]));
+	else
+		tally += ft_puthex_n(h / 16, hexcase) + ft_putchar_n(hexcase[h % 16]);
+	return (tally);
 }
