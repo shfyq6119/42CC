d156a9712c4d4f623b16441539f3a9361a6c02bb
M Syafik M Isa <mm-isa@c2r4s15.42singapore.sg>
2023-11-05 21:33:40 +0800

    mac backup

--- ft_printf/ft_putaddr_n.c
+++ ft_printf/ft_putaddr_n.c
@@ -6,16 +6,19 @@
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:52:32 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/10/06 16:21:11 by mm-isa           ###   ########.fr       */
+/*   Updated: 2023/10/07 12:11:10 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
 int	ft_putaddr_n(unsigned long p)
 {
+	int	tally;
+
+	tally = 0;
 	if (!p)
 		return (ft_putstr_n("(nil)"));
-/*	if (p >= LONG_MAX)
-		return(ft_putstr_n("0x") + ft_puthex_n(UINT_MAX, HEXLOW));*/
-	return (ft_putstr_n("0x") + ft_puthex_n(p, HEXLOW));
+	else
+		tally += ft_putstr_n("0x") + ft_puthex_n(p, HEXLOW);
+	return (tally);
 }
