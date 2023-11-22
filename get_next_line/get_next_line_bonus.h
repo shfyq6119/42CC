da31f1fb05fa3be42e52eeadc41f97b941c3e9fe
M Syafik M Isa <mm-isa@c2r4s15.42singapore.sg>
2023-11-22 10:37:52 +0800

    backup

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:01:56 by mm-isa            #+#    #+#             */
-/*   Updated: 2023/11/08 02:26:26 by mm-isa           ###   ########.fr       */
+/*   Updated: 2023/11/17 10:59:30 by mm-isa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef GET_NEXT_LINE_BONUS_H
@@ -22,8 +22,7 @@
 # include <stdio.h>
 
 char	*get_next_line(int fd);
-char	*ft_strchr(char *str, int chr);
+char	*ft_bzero(int fd, void *buf, size_t bufsize);
 int		ft_schlongth(char *schlong);
-void	ft_bzero(void *buf, size_t bufsize);
 
 #endif
