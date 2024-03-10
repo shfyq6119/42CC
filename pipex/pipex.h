cf19a568f13845400b36362e799e98e0baa79f2a
M Syafik M Isa <mm-isa@c1r3s3.42singapore.sg>
2024-03-11 05:39:44 +0800

    pipex update progress

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -10,17 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PIPEX_H
-#define PIPEX_H
+# define PIPEX_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <errno.h>
 
-#include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
 
-
+void	proc_in(int *file, char *filename1, char *cmd1, char **env);
+void	proc_out(int *file, char *filename2, char *cmd2, char **env);
+void	handle_cmd(char *cmd_raw, char **env);
+void	clean_cache(char **junk);
+void	exit_error(char *str);
+char	**cmd_parse(char *cmd_raw);
+char	*pathfinder(char *cmd, char **envp);
 
 #endif
