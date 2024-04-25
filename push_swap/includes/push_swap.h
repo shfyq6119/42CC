071e820aa8483316897a8c40be272f892e5ed7b1
M Syafik M Isa <mm-isa@c1r6s1.42singapore.sg>
2024-04-26 05:09:48 +0800

    push_swap cost utils added

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -1,17 +1,26 @@
 /* ************************************************************************** */
-
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mm-isa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 22:30:21 by mm-isa            #+#    #+#             */
+/*   Updated: 2024/04/25 22:33:15 by mm-isa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include "../lib_ft/lib_ft.h"
-/*this pathing must always be from the perspective of where the header file lives*/
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "../lib_ft/lib_ft.h"
+/*this pathing must always be from the where the header file lives*/
 
-typedef struct	s_stack
+typedef struct s_stack
 {
 	long			nb;
 	long			idx;
@@ -23,17 +32,17 @@ typedef struct	s_stack
 int		dupchk(t_stack a);
 void	free_load(t_stack a);
 /********************************error  handling*******************************/
-void	//handle_error();
+//void	handle_error();
 void	check_args(int count);
 /*********************************stack parsing********************************/
 t_stack	*stackparse(int ac, char **av);
-t_stack *ft_stacksplitter(char **str);
-int		//ft_chknum(char *str);
-int		ft_stoll_nbrk(char *str);
+t_stack	*ft_stacksplitter(char **str);
+int		ft_chknum(char *str);
+int		ft_atoi_nbrk(char *str);
 /*********************************stack presort********************************/
 void	ft_sortstack(t_stack a);
 int		ft_sortchk(t_stack a);
 /*********************************stack assembly*******************************/
-t_stack	//ft_stack_new(int idx);
-void	//ft_stk_add_last(t_stack *a, t_stack *append);
-
+t_stack	ft_stack_new(int idx);
+void	ft_stk_add_last(t_stack *a, t_stack *append);
+#endif
